023b6b375aa500d9392ee63dc95bc540aae4659e
Abdo <abdo.maxmud@gmail.com>
2024-07-20 20:20:00 +0500

    new build

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/draft-vite/sw.js', { scope: '/draft-vite/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
