4b1bcd07b31401aae7f7f458c5781becb9886c9a
Abdo <abdo.maxmud@gmail.com>
2024-07-20 22:42:38 +0500

    new build

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
+const isProduct = process.env.NODE_ENV === "production" ? true : false;
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../views/Home.vue"),
+  },
+  {
+    path: "/main",
+    name: "main",
+    component: () => import("../views/Main/Main.vue"),
+  },
+];
+
+routes.forEach((item) => {
+  if (isProduct) item.path = `/draft-vite/${item.path}`;
+});
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: () => import("../views/Home.vue"),
-    },
-    {
-      path: "/main",
-      name: "main",
-      component: () => import("../views/Main/Main.vue"),
-    },
-  ],
+  routes: routes,
 });
 
 export default router;
