c9e00d7d42c47af154cd96c1635d089763dec2cb
Abdo <abdo.maxmud@gmail.com>
2024-07-20 20:41:42 +0500

    new build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/draft-vite",
-  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
+  base: "/draft-vite/",
+  plugins: [
+    vue(),
+    // VitePWA({ registerType: "autoUpdate" })
+  ],
 });
