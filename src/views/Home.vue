281cbf59944ec8c581d76d9f94fd6eb871aef011
Abdo <abdo.maxmud@gmail.com>
2024-07-21 17:52:25 +0500

    new build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,10 @@
 <script>
-import api from "../server/api";
-import Pagination from "../components/Pagination/Pagination.vue";
-import DataDropdown from "../components/DataDropdown/DataDropdown.vue";
+import SupplyCounting from "./Supply/SupplyCounting.vue";
 export default {
   name: "Home",
-  components: { Pagination, DataDropdown },
+  components: { SupplyCounting, SupplyCounting },
   data() {
-    return {
-      user: null,
-      users: this.$util.storage("home-users") || {
-        page: 1,
-        per_page: 1,
-        total: 1,
-        total_pages: 1,
-        data: [],
-      },
-    };
+    return {};
   },
   created() {},
   methods: {},
@@ -24,15 +13,5 @@ export default {
 
 <template>
   <h1>Home</h1>
-  <button @click="$refs.modal.toggleModal()">toggle</button>
-
-  <modal ref="modal">
-    <template #header>
-      <h3>header</h3>
-    </template>
-    <template #body> body </template>
-    <template #footer>
-      <i>footer</i>
-    </template>
-  </modal>
+  <SupplyCounting />
 </template>
---
+++ src/views/Supply/SupplyCounting.vue
@@ -0,0 +1,99 @@
+<script>
+export default {
+  name: "SupplyCounting",
+  data() {
+    return {
+      product: {
+        name: "",
+        price: null,
+        quantity: null,
+      },
+      products: [],
+    };
+  },
+  methods: {
+    addProduct(product) {
+      this.products.push(product);
+      this.product = {
+        name: "",
+        price: null,
+        quantity: null,
+      };
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="row gap-3">
+    <div class="col-md-12">
+      <form @submit.prevent="addProduct(product)">
+        <div class="row">
+          <div class="col-5">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="mahsulot"
+              required
+              v-model="product.name"
+            />
+          </div>
+          <div class="col-3">
+            <input
+              type="number"
+              min="0"
+              step="any"
+              class="form-control"
+              placeholder="narx"
+              required
+              v-model="product.price"
+            />
+          </div>
+          <div class="col-2">
+            <input
+              type="number"
+              min="0"
+              step="any"
+              class="form-control"
+              placeholder="miqdor"
+              required
+              v-model="product.quantity"
+            />
+          </div>
+          <div class="col-2">
+            <button class="w-100">qo'shish</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-12">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="table-responsive" style="max-height: 70vh">
+            <table>
+              <thead>
+                <tr>
+                  <th>Mahsulot</th>
+                  <th>Narx</th>
+                  <th>Miqdor</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, i) in products" :key="i">
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.price }}</td>
+                  <td>{{ item.quantity }}</td>
+                  <td>
+                    <button @click="products.splice(i, 1)">x</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="col-md-6"></div>
+      </div>
+    </div>
+  </div>
+</template>
